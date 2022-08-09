9156b4e0f3de44a412abcee370bb42658daf0325
AhmedHassani <ah.jr222@gmail.com>
2022-08-09 23:51:24 +0300

    v1.1

--- src/main/java/com/ahd/api/items/ItemsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ahd.api.items;
-import org.springframework.data.repository.PagingAndSortingRepository;
-import java.util.Optional;
-
-
-
-public interface ItemsRepository extends PagingAndSortingRepository<ItemsModel, Integer> {
-    Optional<ItemsModel> findByName(String name);
-}

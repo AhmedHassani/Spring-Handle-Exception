a9c8f5889aac3ffa3dfe53bd8af9eafa2f5ed512
AhmedHassani <ah.jr222@gmail.com>
2022-08-10 23:44:37 +0300

    v1.3

--- src/main/java/com/ahd/api/hello/HelloController.java
+++ src/main/java/com/ahd/api/hello/HelloController.java
@@ -1,16 +1,15 @@
 package com.ahd.api.hello;
-
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class HelloController {
-
 
         @GetMapping(value = {"/",""})
         public String hello() {
             return "Hello World";
         }
+
 
 }
